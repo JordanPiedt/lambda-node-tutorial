9a7bf0f03d212f061cc989cf98310f72f52b3a9f
Jordan Piedt <jordanpiedt@gmail.com>
2018-07-02 09:34:16 -0500

    setting up folder structure and prepping for slack api

---
+++ src/slack-alerts-reader.js
@@ -0,0 +1,15 @@
+'use strict';
+const fetch = require('node-fetch');
+
+// module.exports.message = async (message) => {
+//   const messageObj = {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       text: message,
+//     }),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   };
+//   await fetch(process.env.SLACK_WEBHOOK_URL, messageObj);
+// };
